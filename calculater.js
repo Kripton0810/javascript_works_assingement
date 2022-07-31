41496673293e4c20fd9ab7325537e128b3f45e1c
Kripton0810 <subhankar0810@gmail.com>
2022-08-01 03:09:19 +0530

    work one

---
+++ calculater.js
@@ -0,0 +1,15 @@
+//make an calculator
+let a = 10;
+let b = 20;
+let c = a + b;
+console.log(c);
+let d = a - b;
+console.log(d);
+let e = a * b;
+console.log(e);
+let f = a / b;
+console.log(f);
+let g = a % b;
+console.log(g);
+let h = a ** b;
+console.log(h);
